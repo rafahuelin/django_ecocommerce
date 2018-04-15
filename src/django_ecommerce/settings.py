aabc46ed40900ff444232ce5b3473d0b51d0f46c
Rafa <rafahuelin@gmail.com>
2018-04-15 14:12:25 +0200

    local static & media files

--- src/django_ecommerce/settings.py
+++ src/django_ecommerce/settings.py
@@ -138,6 +138,15 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static_storage"),
+]
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "static_root")
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "media_root")
+
 
 # Email host configuration from secrets.json
 
