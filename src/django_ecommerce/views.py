3c44eedee6e436c5cca4d8381ff42f1955e47ccd
Rafa <rafahuelin@gmail.com>
2018-04-15 09:34:55 +0200

    template context

--- src/django_ecommerce/views.py
+++ src/django_ecommerce/views.py
@@ -3,7 +3,27 @@ from django.shortcuts import render
 
 
 def home_page(request):
-    return render(request, 'home_page.html', {})
+    context = {
+        "title": "Home page",
+        "content": "Welcome to the home page.",
+    }
+    return render(request, 'home_page.html', context)
+
+
+def about_page(request):
+    context = {
+        "title": "About page",
+        "content": "Welcome to the about page.",
+    }
+    return render(request, 'home_page.html', context)
+
+
+def contact_page(request):
+    context = {
+        "title": "Contact page",
+        "content": "Welcome to the contact page.",
+    }
+    return render(request, 'home_page.html', context)
 
 
 def home_page_old(request):
