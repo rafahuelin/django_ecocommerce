d063c6e39660d39c3fd0da99194ddb637f0aa00a
Rafa <rafahuelin@gmail.com>
2018-04-16 14:43:54 +0200

    move app urls and create product links

---
+++ src/products/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from .views import (
+    ProductListView,
+    ProductDetailSlugView,
+    )
+
+
+urlpatterns = [
+    path('', ProductListView.as_view()),
+    path('<slug:slug>/', ProductDetailSlugView.as_view()),
+]
+
