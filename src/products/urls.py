b4cddb7d424ff32dde014a2cc25e9858b1f120ea
Rafa <rafahuelin@gmail.com>
2018-04-16 22:38:22 +0200

    create navbar

--- src/products/urls.py
+++ src/products/urls.py
@@ -8,7 +8,7 @@ from .views import (
 app_name = 'products'
 
 urlpatterns = [
-    path('', ProductListView.as_view()),
+    path('', ProductListView.as_view(), name='list'),
     path('<slug:slug>', ProductDetailSlugView.as_view(), name='detail'),
 ]
 
