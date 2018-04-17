7e56fa5f655300a8e26f879b99e1323501669ec8
Rafa <rafahuelin@gmail.com>
2018-04-17 17:20:57 +0200

    more integration between django and bootstrap

--- src/django_ecommerce/urls.py
+++ src/django_ecommerce/urls.py
@@ -19,6 +19,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+
 from .views import home_page, about_page, contact_page, login_page, register_page
 
 
