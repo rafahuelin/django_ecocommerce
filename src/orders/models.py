d86af017b137cfc7ce4b757479047f216b40a57e
Rafa <rafahuelin@gmail.com>
2018-04-26 10:06:27 +0200

    order manager
    Signed-off-by: Rafa <rafahuelin@gmail.com>

--- src/orders/models.py
+++ src/orders/models.py
@@ -17,6 +17,25 @@ ORDER_STATUS_CHOICES = (
 # Unique, Random
 
 
+class OrderManager(models.Manager):
+    def new_or_get(self, billing_profile, cart_obj):
+        created = False
+        qs = self.get_queryset().filter(
+            billing_profile=billing_profile,
+            cart=cart_obj,
+            active=True
+        )
+        if qs.count() == 1:
+            obj = qs.first()
+        else:
+            obj = self.model.objects.create(
+                billing_profile=billing_profile,
+                cart=cart_obj
+            )
+            created = True
+        return obj, created
+
+
 class Order(models.Model):
     billing_profile = models.ForeignKey(BillingProfile, on_delete=models.CASCADE, null=True, blank=True)
     order_id = models.CharField(max_length=120, blank=True)
@@ -32,6 +51,8 @@ class Order(models.Model):
     def __str__(self):
         return self.order_id
 
+    objects = OrderManager()
+
     def update_total(self):
         cart_total = self.cart.total
         shipping_total = self.shipping_total
@@ -46,6 +67,9 @@ class Order(models.Model):
 def pre_save_create_order_id(sender, instance, *args, **kwargs):
     if not instance.order_id:
         instance.order_id = unique_order_id_generator(instance)
+    qs = Order.objects.filter(cart=instance.cart).exclude(billing_profile=instance.billing_profile)
+    if qs.exists():
+        qs.update(active=False)
 
 
 pre_save.connect(pre_save_create_order_id, sender=Order)
