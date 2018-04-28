6b19ae9a81378ed4019a85b216c05d15106eb79e
Rafa <rafahuelin@gmail.com>
2018-04-28 23:45:04 +0200

    cart item count ajax
    Signed-off-by: Rafa <rafahuelin@gmail.com>

--- src/carts/views.py
+++ src/carts/views.py
@@ -41,7 +41,8 @@ def cart_update(request):
             print("Ajax request")
             json_data = {
                 "added": added,
-                "removed": not added
+                "removed": not added,
+                "cartItemCount": cart_obj.products.count()
             }
             return JsonResponse(json_data)
     return redirect("cart:home")
