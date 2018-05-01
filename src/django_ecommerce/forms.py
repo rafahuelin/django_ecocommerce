2fa6c69b0a77c533272d15c4bfdc3fc9a58ca116
Rafa <rafahuelin@gmail.com>
2018-05-01 11:46:18 +0200

    move custom js to its own file
    Signed-off-by: Rafa <rafahuelin@gmail.com>

--- src/django_ecommerce/forms.py
+++ src/django_ecommerce/forms.py
@@ -24,11 +24,14 @@ class ContactForm(forms.Form):
         }
     ))
 
-    def clean_email(self):
-        email = self.cleaned_data.get("email")
-        if not "gmail.com" in email:  # TODO: validation that usually should be changed
-            raise forms.ValidationError("Email has to be gmail.com")
-        return email
+    # def clean_email(self):
+    #     email = self.cleaned_data.get("email")
+    #     if not "gmail.com" in email:
+    #         raise forms.ValidationError("Email has to be gmail.com")
+    #     return email
+
+    # def clean_content(self):
+    #     raise forms.ValidationError("Content is wrong.")
 
 
 class LoginForm(forms.Form):
