e05dc4373a2f3ae6484d3ba8033125cd0f7e5c09
Rafa <rafahuelin@gmail.com>
2018-05-05 12:20:33 +0200

    guest card checkout
    Signed-off-by: Rafa <rafahuelin@gmail.com>

--- src/billing/models.py
+++ src/billing/models.py
@@ -65,6 +65,11 @@ class BillingProfile(models.Model):
             return default_cards.first()
         return None
 
+    def set_cards_inactive(self):
+        cards_qs = self.get_cards()
+        cards_qs.update(active=False)
+        return cards_qs.filter(active=True).count()
+
 
 def billing_profile_created_receiver(sender, instance, *args, **kwargs):
     if not instance.customer_id and instance.email:
@@ -88,6 +93,9 @@ post_save.connect(user_created_receiver, sender=User)
 
 
 class CardManager(models.Manager):
+    def all(self, *args, **kwargs):
+        return self.get_queryset().filter(active=True)
+
     def add_new(self, billing_profile, token):
         if token:
             customer = stripe.Customer.retrieve(billing_profile.customer_id)
@@ -115,19 +123,13 @@ class Card(models.Model):
     exp_year = models.IntegerField(null=True, blank=True)
     last4 = models.CharField(max_length=4, null=True, blank=True)
     default = models.BooleanField(default=True)
+    active = models.BooleanField(default=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     objects = CardManager()
 
     def __str__(self):
         return f"{self.brand} {self.last4}"
-
-
-# stripe.Charge.create(
-#     amount=2000,
-#     currency="eur",
-#     source="tok_visa",  # obtained with Stripe.js
-#     description="Charge for emily.thompson@example.com"
-# )
 
 
 class ChargeManager(models.Manager):
