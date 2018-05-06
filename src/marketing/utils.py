7091133648a55ad1dffd568d2ee95130367d0a4a
Rafa <rafahuelin@gmail.com>
2018-05-06 15:20:06 +0200

    add more functionality to the Mailchimp class
    Signed-off-by: Rafa <rafahuelin@gmail.com>

--- src/marketing/utils.py
+++ src/marketing/utils.py
@@ -1,4 +1,6 @@
+import hashlib
 import json
+import re
 import requests
 
 from django.conf import settings
@@ -6,6 +8,19 @@ from django.conf import settings
 MAILCHIMP_API_KEY = getattr(settings, "MAILCHIMP_API_KEY", None)
 MAILCHIMP_DATA_CENTER = getattr(settings, "MAILCHIMP_DATA_CENTER", None)
 MAILCHIMP_EMAIL_LIST_ID = getattr(settings, "MAILCHIMP_EMAIL_LIST_ID", None)
+
+
+def check_email(email):
+    if not re.match(r".+@.+\..+", email):
+        raise ValueError('String passed is not a valid email address')
+    return email
+
+
+def get_subscriber_hash(member_email):
+    check_email(member_email)
+    member_email = member_email.lower().encode()
+    m = hashlib.md5(member_email)
+    return m.hexdigest()
 
 
 class Mailchimp(object):
@@ -19,12 +34,33 @@ class Mailchimp(object):
             list_id=self.list_id
         )
 
+    def get_members_endpoint(self):
+        return self.list_endpoint + "/members"
+
+    def change_subscription_status(self, email, status='unsubscribed'):  # , check_status=False
+        # endpoint
+        # method
+        # data
+        # auth
+        hashed_email = get_subscriber_hash(email)
+        print(hashed_email)
+        endpoint = self.get_members_endpoint() + "/" + hashed_email
+        data = {
+            "status": self.check_valid_status(status)
+        }
+        # if check_status:
+        #     return requests.get(endpoint, auth=("", self.key)).json()
+        r = requests.put(endpoint, auth=("", self.key), data=json.dumps(data))
+        return r.json()
+
     def check_subscription_status(self, email):
         # endpoint
         # method
         # data
         # auth
-        endpoint = self.api_url
+        hashed_email = get_subscriber_hash(email)
+        print(hashed_email)
+        endpoint = self.get_members_endpoint() + "/" + hashed_email
         r = requests.get(endpoint, auth=("", self.key))
         return r.json()
 
@@ -35,16 +71,27 @@ class Mailchimp(object):
         return status
 
     def add_email(self, email):
-        # endpoint
-        # method
-        # data
-        # auth
-        status = "subscribed"
-        self.check_valid_status(status)
-        data = {
-            "email_address": email,
-            "status": status
-        }
-        endpoint = self.list_endpoint + "/members"
-        r = requests.post(endpoint, auth=("", self.key), data=json.dumps(data))
-        return r.json()
+        # # endpoint
+        # # method
+        # # data
+        # # auth
+        # status = "subscribed"
+        # self.check_valid_status(status)
+        # data = {
+        #     "email_address": email,
+        #     "status": status
+        # }
+        # endpoint = self.get_members_endpoint()
+        # r = requests.post(endpoint, auth=("", self.key), data=json.dumps(data))
+        # return r.json()
+        return self.change_subscription_status(email, status='subscribed')
+        # this returned code does the same as the commented code
+
+    def unsubscribe(self, email):
+        return self.change_subscription_status(email, status='unsubscribed')
+
+    def subscribe(self, email):
+        return self.change_subscription_status(email, status='subscribed')
+
+    def pending(self, email):
+        return self.change_subscription_status(email, status='pending')
